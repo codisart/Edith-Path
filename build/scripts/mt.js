39c5ed9bd921083be2a7a189a580de4d97438cde
codisart <louis.celier@gmail.com>
2015-12-03 19:27:15 +0100

    [TECH] the one where the lint is fixed

--- build/scripts/mt.js
+++ build/scripts/mt.js
@@ -4,7 +4,7 @@ var ChildProcess = require('child_process');
 
 var manifestPath = "../../assets/Edith_Path.exe.manifest";
 var outputResourcePath = "../../release/Edith-path-win32-x64/Edith-path.exe";
-var mtCommand = "mt.exe -manifest " + manifestPath + " -outputresource:" + outputResourcePath
+var mtCommand = "mt.exe -manifest " + manifestPath + " -outputresource:" + outputResourcePath;
 
 
 ChildProcess.exec("cd build/bin/" + process.arch + " & " + mtCommand, function (error, stdout, stderr) {
