434426fe7528817de1313f596685f48691636fcd
codisart <louis.celier@gmail.com>
2015-12-03 19:22:53 +0100

    [TECH] Correction des scripts de build de l'app.

---
+++ build/scripts/mt.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var ChildProcess = require('child_process');
+
+var manifestPath = "../../assets/Edith_Path.exe.manifest";
+var outputResourcePath = "../../release/Edith-path-win32-x64/Edith-path.exe";
+var mtCommand = "mt.exe -manifest " + manifestPath + " -outputresource:" + outputResourcePath
+
+
+ChildProcess.exec("cd build/bin/" + process.arch + " & " + mtCommand, function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+		console.log('exec error: ' + error);
+	}
+});
