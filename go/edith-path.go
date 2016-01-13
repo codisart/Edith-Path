da42d3be395ea1b3f56937d7a932b814c138c773
codisart <louis.celier@gmail.com>
2016-01-13 18:18:51 +0100

    [TECH] the one where the err message are deleted

--- go/edith-path.go
+++ go/edith-path.go
@@ -11,23 +11,19 @@ func main() {
 
 	if (newPathStringValue != "") {
 		registryKey, err := registry.OpenKey(registry.LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", registry.SET_VALUE)
-
 		if (err != nil) {
-			os.Stderr.WriteString(err.Error())
 			os.Exit(1)
 		}
 
 		defer registryKey.Close()
 
 		err2 := registryKey.SetExpandStringValue("testPath" , newPathStringValue)
-
 		if (err2 != nil) {
-			os.Stderr.WriteString(err2.Error())
 			os.Exit(1)
 		}
+		
 		os.Exit(0)
 	}
 
-	os.Stderr.WriteString("This program needs a argument.")
 	os.Exit(1)
 }
