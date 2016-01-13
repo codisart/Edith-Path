c94a26205023e556ff8a249e515990be75d8b134
codisart <louis.celier@gmail.com>
2016-01-13 15:38:52 +0100

    [TECH] The one where the go can take one argument

--- go/test.go
+++ go/test.go
@@ -2,23 +2,32 @@ package main
 
 import (
 	"os"
-	"fmt"
 	"golang.org/x/sys/windows/registry"
 )
 
 func main() {
 
-	registryKey, err := registry.OpenKey(registry.LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", registry.SET_VALUE)
+	newPathStringValue := os.Args[1]
 
-	if err != nil {
-		os.Stderr.WriteString(err.Error())
+	if (newPathStringValue != "") {
+		registryKey, err := registry.OpenKey(registry.LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", registry.SET_VALUE)
+
+		if (err != nil) {
+			os.Stderr.WriteString(err.Error())
+			os.Exit(1)
+		}
+
+		defer registryKey.Close()
+
+		err2 := registryKey.SetExpandStringValue("testPath" , newPathStringValue)
+
+		if (err2 != nil) {
+			os.Stderr.WriteString(err2.Error())
+			os.Exit(1)
+		}
+		os.Exit(0)
 	}
 
-	defer registryKey.Close()
-
-	err2 := registryKey.SetExpandStringValue("Path" , "%SystemRoot%\\system32;%SystemRoot%;%SystemRoot%\\System32\\Wbem;%SYSTEMROOT%\\System32\\WindowsPowerShell\\v1.0\\;C:\\Program Files\\OpenSSH\\bin;C:\\Users\\IEUser\\Downloads;C:\\Users\\IEUser\\Downloads;C:\\Users\\IEUser\\Downloads\\test;C:\\MinGW\\bin;C:\\Go\\bin;C:\\Git\\bin;C:\\artoo")
-
-	if err2 != nil {
-		os.Stderr.WriteString(err2.Error())
-	}
+	os.Stderr.WriteString("This program needs a argument.")
+	os.Exit(1)
 }
