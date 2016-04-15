e215836c24e3f19696c3874fc8f20b4f8732e087
codisart <louis.celier@gmail.com>
2016-04-15 15:02:16 +0200

    [DOC] Replace all tabs by spaces indentation

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ var Application = require('app');  // Module to control application life.
 var OperatingSystem = require('os');
 
 try {
-	var CONF = require('./config/dev.js');
+    var CONF = require('./config/dev.js');
 }
 catch (e) {
-	try {
-		var CONF = require('./config/release.js');
-	}
-	catch (e) {
-		var CONF = require('./config/setup.js');
-	}
+    try {
+        var CONF = require('./config/release.js');
+    }
+    catch (e) {
+        var CONF = require('./config/setup.js');
+    }
 }
 
 if (!CONF.isDevelopmentEnvironment && OperatingSystem.platform() != 'win32') {
@@ -37,156 +37,156 @@ var regKey = new Winreg({
 var executableName = 'edith-path.exe';
 
 if (process.argv[1] && process.argv[0].toLowerCase().substr(-executableName.length) === executableName) {
-	FileSystem.stat(process.argv[1], function(err, stats) {
-		if (err) {
-			Application.quit();
-		}
-		if(stats.isDirectory()) {
-			regKey.get('Path', function(err, item) {
-				if (err) {
-					Application.quit();
-				}
-				try {
-          var newPathStringValue = '"\\"' + PathParser.addCarat(item.value) + ';' + process.argv[1] + '"\\""';
-					ChildProcess.exec("cd " + CONF.golang.path + " & edith-path.exe " + newPathStringValue, function (error, stdout, stderr) {
-						Application.quit();
-					});
-				}
-				catch (e) {
-					Application.quit();
-				}
-			});
-		}
-	});
+    FileSystem.stat(process.argv[1], function(err, stats) {
+        if (err) {
+            Application.quit();
+        }
+        if(stats.isDirectory()) {
+            regKey.get('Path', function(err, item) {
+                if (err) {
+                    Application.quit();
+                }
+                try {
+                    var newPathStringValue = '"\\"' + PathParser.addCarat(item.value) + ';' + process.argv[1] + '"\\""';
+                    ChildProcess.exec("cd " + CONF.golang.path + " & edith-path.exe " + newPathStringValue, function (error, stdout, stderr) {
+                        Application.quit();
+                    });
+                }
+                catch (e) {
+                    Application.quit();
+                }
+            });
+        }
+    });
 } else {
-	var BrowserWindow = require('browser-window');  // Module to create native browser window.
-	const IpcMain = require('electron').ipcMain;
+    var BrowserWindow = require('browser-window');  // Module to create native browser window.
+    const IpcMain = require('electron').ipcMain;
 
-	// Keep a global reference of the window object, if you don't, the window will
-	// be closed automatically when the JavaScript object is garbage collected.
-	var mainWindow = null;
+    // Keep a global reference of the window object, if you don't, the window will
+    // be closed automatically when the JavaScript object is garbage collected.
+    var mainWindow = null;
 
-	// Quit when all windows are closed.
-	Application.on('window-all-closed', function() {
-	    Application.quit();
-	});
+    // Quit when all windows are closed.
+    Application.on('window-all-closed', function() {
+        Application.quit();
+    });
 
-	// This method will be called when Electron has finished
-	// initialization and is ready to create browser windows.
-	Application.on('ready', function() {
-	    // Create the browser window.
-	    mainWindow = new BrowserWindow({
-	        width: 750,
-	        height: 650,
-	        icon: 'img/icon.png',
-	    });
-	    // mainWindow.setResizable(false);
+    // This method will be called when Electron has finished
+    // initialization and is ready to create browser windows.
+    Application.on('ready', function() {
+        // Create the browser window.
+        mainWindow = new BrowserWindow({
+            width: 750,
+            height: 650,
+            icon: 'img/icon.png',
+        });
+        // mainWindow.setResizable(false);
 
-	    var webContents = mainWindow.webContents;
+        var webContents = mainWindow.webContents;
 
-	    // and load the index.html of the app.
-	    mainWindow.loadURL('file://' + __dirname + '/index.html');
+        // and load the index.html of the app.
+        mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-	    webContents.on('did-finish-load', function() {
-	        regKey.get('Path', function(err, item) {
-	            if (!err) {
-					buildHtmlData(PathParser.parseString(item.value)).then(
-						function (values) {
-	                		webContents.send('data-folders', values);
-						}
-					);
-	            }
-	        });
-	    });
+        webContents.on('did-finish-load', function() {
+            regKey.get('Path', function(err, item) {
+                if (!err) {
+                    buildHtmlData(PathParser.parseString(item.value)).then(
+                        function (values) {
+                            webContents.send('data-folders', values);
+                        }
+                    );
+                }
+            });
+        });
 
-	    IpcMain.on('add-folders', function(event, arg) {
-	        regKey.get('Path', function(err, item) {
-	            if (!err) {
-	                try {
-	                    var pathArrayValue = PathParser.parseArray(arg);
+        IpcMain.on('add-folders', function(event, arg) {
+            regKey.get('Path', function(err, item) {
+                if (!err) {
+                    try {
+                        var pathArrayValue = PathParser.parseArray(arg);
                         var newPathStringValue = '"\\"' + PathParser.addCarat(item.value) + ';' + pathArrayValue + '"\\""';
-						ChildProcess.exec("cd " + CONF.golang.path + " & edith-path.exe " + newPathStringValue, function (error, stdout, stderr) {
-							if (error) {
-								console.log('exec error: ' + error);
-							} else {
-								buildHtmlData(PathParser.parseString(item.value)).then(
-									function (values) {
-			                    		webContents.send('data-folders', values);
-									}
-								);
-							}
-						});
-	                }
-	                catch (e) {
-	                    event.sender.send('error-paths', 'plop');
-	                }
-	            }
-	        });
-	    });
+                        ChildProcess.exec("cd " + CONF.golang.path + " & edith-path.exe " + newPathStringValue, function (error, stdout, stderr) {
+                            if (error) {
+                                console.log('exec error: ' + error);
+                            } else {
+                                buildHtmlData(PathParser.parseString(item.value)).then(
+                                    function (values) {
+                                        webContents.send('data-folders', values);
+                                    }
+                                );
+                            }
+                        });
+                    }
+                    catch (e) {
+                        event.sender.send('error-paths', 'plop');
+                    }
+                }
+            });
+        });
 
-	    IpcMain.on('refresh-folders', function(event, arg) {
-	        regKey.get('Path', function(err, item) {
-	            if (!err) {
-					buildHtmlData(PathParser.parseString(item.value)).then(
-						function (values) {
-	                		webContents.send('data-folders', values);
-						}
-					);
-	            }
-	        });
-	    });
+        IpcMain.on('refresh-folders', function(event, arg) {
+            regKey.get('Path', function(err, item) {
+                if (!err) {
+                    buildHtmlData(PathParser.parseString(item.value)).then(
+                        function (values) {
+                            webContents.send('data-folders', values);
+                        }
+                    );
+                }
+            });
+        });
 
-	    function buildHtmlData(pathArrayValue) {
-	        var folders = [];
+        function buildHtmlData(pathArrayValue) {
+            var folders = [];
 
-	        var arrayLength = pathArrayValue.length;
-	        for (var i = 0; i < arrayLength; i++) {
-	            var folder = {
-					path: pathArrayValue[i],
-					exists: false,
-				};
-				folders.push(folder);
-			}
+            var arrayLength = pathArrayValue.length;
+            for (var i = 0; i < arrayLength; i++) {
+                var folder = {
+                    path: pathArrayValue[i],
+                    exists: false,
+                };
+                folders.push(folder);
+            }
 
-			var promises = folders.map(function(folder) {
-				return new Promise(function (resolve, reject) {
-					FileSystem.stat(folder.path, function(err, stats) {
-						if(err) {
-							ChildProcess.exec("echo " + folder.path, function(stderr, stdout, err) {
-								if (!err && folder.path != stdout.trim()) {
-									FileSystem.stat(stdout.trim(), function(err, stats) {
-										if(err) {
-											folder.exists = false;
-										} else {
-											folder.exists = stats.isDirectory();
-										}
-										resolve(folder);
-									});
-								}
-								folder.exists = false;
-								resolve(folder);
-							});
-						} else {
-							folder.exists = stats.isDirectory();
-							resolve(folder);
-						}
-					});
-				}).then(
-					function (folder) {
-						return folder;
-					}
-				);
-			});
+            var promises = folders.map(function(folder) {
+                return new Promise(function (resolve, reject) {
+                    FileSystem.stat(folder.path, function(err, stats) {
+                        if(err) {
+                            ChildProcess.exec("echo " + folder.path, function(stderr, stdout, err) {
+                                if (!err && folder.path != stdout.trim()) {
+                                    FileSystem.stat(stdout.trim(), function(err, stats) {
+                                        if(err) {
+                                            folder.exists = false;
+                                        } else {
+                                            folder.exists = stats.isDirectory();
+                                        }
+                                        resolve(folder);
+                                    });
+                                }
+                                folder.exists = false;
+                                resolve(folder);
+                            });
+                        } else {
+                            folder.exists = stats.isDirectory();
+                            resolve(folder);
+                        }
+                    });
+                }).then(
+                    function (folder) {
+                        return folder;
+                    }
+                );
+            });
 
-			return Promise.all(promises);
-	    }
+            return Promise.all(promises);
+        }
 
-	    // Emitted when the window is closed.
-	    mainWindow.on('closed', function() {
-	        // Dereference the window object, usually you would store windows
-	        // in an array if your app supports multi windows, this is the time
-	        // when you should delete the corresponding element.
-	        mainWindow = null;
-	    });
-	});
+        // Emitted when the window is closed.
+        mainWindow.on('closed', function() {
+            // Dereference the window object, usually you would store windows
+            // in an array if your app supports multi windows, this is the time
+            // when you should delete the corresponding element.
+            mainWindow = null;
+        });
+    });
 }
