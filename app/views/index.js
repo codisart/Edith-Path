782a8c918a0496d8befbf4cc77456f918d4af606
codisart <louis.celier@gmail.com>
2016-04-20 17:24:00 +0200

    [DOC] Replace all tabs by spaces in index.js

--- app/views/index.js
+++ app/views/index.js
@@ -10,22 +10,22 @@ window.$ = window.jQuery = require('jquery');
 var foldersToAdd = [];
 
 document.ondragover = document.ondrop = function(e) {
-  e.preventDefault();
-  return false;
+    e.preventDefault();
+    return false;
 };
 
 function addFolder(path) {
-	if(path) {
-		foldersToAdd.push(path);
+    if(path) {
+        foldersToAdd.push(path);
 
-		var folderElementHtml = $('<li/>');
+        var folderElementHtml = $('<li/>');
 
-		folderElementHtml
-			.attr('class', 'valid-path icon-teal')
-			.html(path);
+        folderElementHtml
+            .attr('class', 'valid-path icon-teal')
+            .html(path);
 
-		$('#folders-to-add-list').prepend(folderElementHtml);
-	}
+        $('#folders-to-add-list').prepend(folderElementHtml);
+    }
 }
 
 function makeDoubleClickHandler() {
@@ -36,58 +36,58 @@ function makeDoubleClickHandler() {
 }
 
 IpcRenderer.on('data-folders', function(event, folders) {
-	var arrayLength = folders.length;
-	var foldersListHtml = $('#folders-list').empty();
+    var arrayLength = folders.length;
+    var foldersListHtml = $('#folders-list').empty();
 
-	if(arrayLength > 0) {
-		for (var i = 0; i < arrayLength; i++) {
-			var folderElementHtml = $('<li/>');
+    if(arrayLength > 0) {
+        for (var i = 0; i < arrayLength; i++) {
+            var folderElementHtml = $('<li/>');
 
-			if(folders[i].exists) {
-				folderElementHtml
+            if(folders[i].exists) {
+                folderElementHtml
                     .attr('class', 'valid-path icon-teal')
                     .dblclick(makeDoubleClickHandler());
-			} else {
-				folderElementHtml
+            } else {
+                folderElementHtml
                     .attr('class', 'invalid-path icon-red');
-			}
+            }
 
-			folderElementHtml
-				.html(folders[i].path);
-				// .prepend(iconHtml);
-			$('.loader').hide();
-			foldersListHtml.prepend(folderElementHtml);
-		}
-	} else {
-		foldersListHtml.after($('<p/>').html('Error'));
-	}
+            folderElementHtml
+                .html(folders[i].path);
+                // .prepend(iconHtml);
+            $('.loader').hide();
+            foldersListHtml.prepend(folderElementHtml);
+        }
+    } else {
+        foldersListHtml.after($('<p/>').html('Error'));
+    }
 });
 
 $('.choose-folder').on('click', function() {
-	var folderPath = Dialog.showOpenDialog({ properties: [ 'openDirectory']});
+    var folderPath = Dialog.showOpenDialog({ properties: [ 'openDirectory']});
 
-	if(folderPath.length > 0){
-		addFolder(folderPath[0]);
-	}
+    if(folderPath.length > 0){
+        addFolder(folderPath[0]);
+    }
 });
 
 $('.folder-input button').on('click', function() {
-	IpcRenderer.send('add-folders', foldersToAdd);
-	$('.loader').show();
-	$('#folders-to-add-list').empty();
-	return false;
+    IpcRenderer.send('add-folders', foldersToAdd);
+    $('.loader').show();
+    $('#folders-to-add-list').empty();
+    return false;
 });
 
 $('#holder').on("drop", function(event) {
     event.preventDefault();
-	event.stopPropagation();
+    event.stopPropagation();
     console.log('success');
-	var folderPath = event.originalEvent.dataTransfer.files[0].path;
+    var folderPath = event.originalEvent.dataTransfer.files[0].path;
 
-	addFolder(folderPath);
+    addFolder(folderPath);
 });
 
 $('.refresh-list').on("click", function() {
-	IpcRenderer.send('refresh-folders', true);
-	$('.loader').show();
+    IpcRenderer.send('refresh-folders', true);
+    $('.loader').show();
 });
